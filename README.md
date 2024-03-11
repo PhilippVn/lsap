a1cd4dfe0acc9eaea2a56273a5d0a15685aee70f
Philipp von Neffe <philipp.von-neffe@tum.de>
2024-03-11 20:08:14 +0100

    cmake project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a app to list all available access points in your region
---
+++ src/lsap.cpp
@@ -0,0 +1,7 @@
+#include "lsap.hpp"
+#include <iostream>
+
+int main(int argc,char* argv[]){
+    std::cout << "Hello World!" << std::endl;
+}
+
