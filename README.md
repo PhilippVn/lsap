39c3d4fffc7b776ba399cd4246f071c76b95db40
Philipp von Neffe <philipp.von-neffe@tum.de>
2024-03-13 02:32:51 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is a app to list all available access points in your region
+
+**RFC:**
+https://www.rfc-editor.org/info/rfc5416
--- src/lsap.cpp
+++ src/lsap.cpp
@@ -1,7 +1,14 @@
 #include "lsap.hpp"
 #include <iostream>
+#include <memory>
+#include <cstdint>
+
+// TODO
+// Identify all avaiable access points
+// Get their ssids, security, signal strength, download and upload rate
+// sort by security and signalstrength
 
 int main(int argc,char* argv[]){
-    std::cout << "Hello World!" << std::endl;
+    
 }
 
